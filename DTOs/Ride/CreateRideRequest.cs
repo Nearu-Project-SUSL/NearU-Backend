2b5a92b9ec175b52996695c71373f62eceedc2b4
pamal pahasara <pamalpahasara@gmail.com>
2026-05-09 10:32:09 +0530

    ride dtos for response, create, accept and verifyotp

--- DTOs/Ride/CreateRideRequest.cs
+++ DTOs/Ride/CreateRideRequest.cs
@@ -1,13 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NearU_Backend_Revised.DTOs.Rides
 {
     public class CreateRideRequestDto
     {
-        public Guid UserId { get; set; }
-        public string ServiceType { get; set; } = string.Empty;
-        public string Details { get; set; } = string.Empty;
+        [Required]
+        public Guid StudentId { get; set; }
+
+        [Required]
+        public string ServiceType { get; set; } = string.Empty; // PersonalRide, FoodDelivery, GroceryPickup
+
+        public string? Details { get; set; }
+
+        [Required]
         public double PickupLat { get; set; }
+
+        [Required]
         public double PickupLon { get; set; }
+
+        [Required]
         public double DropoffLat { get; set; }
+
+        [Required]
         public double DropoffLon { get; set; }
     }
 }
