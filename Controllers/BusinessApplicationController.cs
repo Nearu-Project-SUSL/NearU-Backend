4c4d4255e1af8a8551cc10f6d6d39e58db43e89f
pamal pahasara <pamalpahasara@gmail.com>
2026-05-30 11:08:27 +0530

    controller and dto for business application submittion

---
+++ Controllers/BusinessApplicationController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NearU_Backend_Revised.Data;
+using NearU_Backend_Revised.DTOs.Business;
+using NearU_Backend_Revised.Models;
+
+namespace NearU_Backend_Revised.Controllers;
+
+[ApiController]
+[Route("api/business-applications")]
+[Authorize]
+public class BusinessApplicationController : ControllerBase
+{
+  public readonly ApplicationDbContext _db;
+
+  public BusinessApplicationController(ApplicationDbContext db)
+  {
+    _db = db;
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Submit(
+    [FromBody] CreateBusinessApplication dto)
+  {
+    var userId = User.FindFirst("userId")?.Value;
+
+    if(string.IsNullOrEmpty(userId))
+      return Unauthorized();
+
+    var application = new BusinessApplication
+    {
+      UserId = userId,
+      BusinessType = dto.BusinessType,
+      BusinessName = dto.BusinessName,
+      OwnerName = dto.OwnerName,
+      Phone = dto.Phone,
+      Address = dto.Address,
+      Description = dto.Description,
+      RegistrationNumber = dto.RegistrationNumber,
+      ApplicationDataJson = dto.ApplicationDataJson,
+      Status = "Pending"
+    };
+
+    _db.BusinessApplications.Add(application);
+
+    await _db.SaveChangesAsync();
+
+    return Ok(
+      ApiResponse<object>.SuccessResponse(
+        "Application submitted successfully",
+        application.Id
+      )
+      
+    );
+  } 
+}
---
+++ DTOs/Auth/CreateBusinessApplication.cs
@@ -0,0 +1,20 @@
+namespace NearU_Backend_Revised.DTOs.Business;
+
+public class CreateBusinessApplication
+{
+    public string BusinessType { get; set; } = string.Empty;
+
+    public string BusinessName { get; set; } = string.Empty;
+
+    public string OwnerName { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string RegistrationNumber { get; set; } = string.Empty;
+
+    public string ApplicationDataJson { get; set; } = "{}";
+}
