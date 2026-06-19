78b914cd200a4e7b53346fc48466936cf4d68d03
pamal pahasara <pamalpahasara@gmail.com>
2026-06-19 21:02:16 +0530

    add tables to db

--- Program.cs
+++ Program.cs
@@ -234,12 +234,10 @@ builder.Services.AddScoped<IAccommodationItemRepository, AccommodationItemReposi
 builder.Services.AddScoped<IAccommodationService, AccommodationService>();
 builder.Services.AddScoped<IAccommodationItemService, AccommodationItemService>();
 
-//Transport Repositories
+//Transport 
 builder.Services.AddScoped<ITukTukDriverRepository, TukTukDriverRepository>();
 builder.Services.AddScoped<IBusRouteRepository, BusRouteRepository>();
 builder.Services.AddScoped<ITrainRouteRepository, TrainRouteRepository>();
-
-// Services
 builder.Services.AddScoped<ITukTukDriverService, TukTukDriverService>();
 builder.Services.AddScoped<IBusRouteService, BusRouteService>();
 builder.Services.AddScoped<ITrainRouteService, TrainRouteService>();
