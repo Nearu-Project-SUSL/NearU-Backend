f589dd80d7c92f913e3b7504218e1d6fbf8dd59f
Thimira Niranjaya <Thimira Niranjaya>
2026-08-10 21:19:18 +0530

    feat: implement UserService and remove SendGrid dependency in favor of ResendEmailService

--- Configuration/SendGridSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace NearU_Backend_Revised.Configuration
-{
-    public class SendGridSettings
-    {
-        public string ApiKey { get; set; } = null!;
-        public string FromEmail { get; set; } = null!;
-        public string FromName { get; set; } = null!;
-    }
-}
